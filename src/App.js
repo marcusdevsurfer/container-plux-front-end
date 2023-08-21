bfcc0ee3b5b1a1bfa710a723a4360eb9cb514ee1
marcusdev99 <marcus.sea13@gmail.com>
2023-08-20 23:20:28 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Navbar } from "./components/Navbar";
+import { ContainerInventory } from "./components/ContainerInventory";
+import { AddContainer } from "./components/AddContainer";
+
+
+
+
+
+function App() {
+
+
+  return (
+    <h1>Initial commit</h1>
+  );
+}
+
+export default App;
