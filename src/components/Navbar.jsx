a4524422d283b764d67314d2719cb3cfa54477ba
marcusdev99 <marcus.sea13@gmail.com>
2023-08-21 16:10:23 -0600

    feat : addition of Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand">
+            Logo empresa
+          </a>
+        </div>
+      </nav>
+  )
+}
