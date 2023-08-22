06afc9ada9bdb19c53fda58ee42740f49d2312f3
marcusdev99 <marcus.sea13@gmail.com>
2023-08-22 14:15:04 -0600

    refactor : addition of component unificated -TitleContainerInventory -ButtonsContainerInventory

--- src/components/ContainerInventory.jsx
+++ src/components/ContainerInventory.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { ButtonsContainerInventory } from './ButtonsContainerInventory';
+import { TitleContainerInventory } from './TitleContainerInventory';
 
 export const ContainerInventory = () => {
 
@@ -24,74 +26,76 @@ export const ContainerInventory = () => {
 
     return (
 
-        <div className='container'>
+            <div className='container'>
 
-            <div className="my-3">
-                <h1 className="text-center display-6 p-1">Inventario</h1>
+                <TitleContainerInventory/>
+                <ButtonsContainerInventory />
+
+                {data.length <= 0
+                    ?
+                    <h3 className='text-center display-'>No hay registros!</h3>
+                    :
+                    <div className="my-4">
+                        <table className="table text-center">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Fecha ingreso</th>
+                                    <th>Hora de ingreso</th>
+                                    <th>Naviera</th>
+                                    <th>Estado</th>
+                                    <th>Tipo</th>
+                                    <th>Tamaño</th>
+                                    <th>Operador</th>
+                                    <th>Placas</th>
+                                    <th>Transportista</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.map((container) => (
+                                    <tr key={container.id}>
+                                        <td>
+                                            {container.id}
+                                        </td>
+                                        <td>
+                                            {container.entryDay}
+                                        </td>
+                                        <td>
+                                            {container.entryHour}
+                                        </td>
+                                        <td>
+                                            {container.shippingCompany}
+                                        </td>
+                                        <td>
+                                            {container.status === "empty" ? "Vacio" : "Lleno"}
+                                        </td>
+                                        <td>
+                                            {container.type}
+                                        </td>
+                                        <td>
+                                            {container.size}
+                                        </td>
+                                        <td>
+                                            {container.operator}
+                                        </td>
+                                        <td>
+                                            {container.licencePlate}
+                                        </td>
+                                        <td>
+                                            {container.carrier}
+                                        </td>
+                                        <td>
+                                            <button className='btn btn-secondary btn-sm mx-1'>Editar</button>
+                                            <button className='btn btn-danger btn-sm mx-1'>Ver EIR</button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                }
+
             </div>
-
-
-            <div className="text-center my-2">
-                <button className="btn btn-success p-2">Agregar contenedor</button>
-            </div>
-
-            <div className="text-center my-2">
-                <button className='btn btn-primary'>Activos</button>
-                <button className="btn btn-outline-secondary p-1 m-1">Llenos</button>
-                <button className="btn btn-outline-secondary p-1 m-1">Vacios</button>
-            </div>
-
-            <div className="my-4">
-                <table className="table text-center table-sm table-success">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Fecha ingreso</th>
-                            <th>Hora de ingreso</th>
-                            <th>Naviera</th>
-                            <th>Tipo</th>
-                            <th>Tamaño</th>
-                            <th>Operador</th>
-                            <th>Placas</th>
-                            <th>Transportista</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((container) => (
-                            <tr key={container.id} className="table-light">
-                                <td>
-                                    {container.id}
-                                </td>
-                                <td>
-                                    {container.entryDay}
-                                </td>
-                                <td>
-                                    {container.entryHour}
-                                </td>
-                                <td>
-                                    {container.shippingCompany}
-                                </td>
-                                <td>
-                                    {container.type}
-                                </td>
-                                <td>
-                                    {container.size}
-                                </td>
-                                <td>
-                                    {container.operator}
-                                </td>
-                                <td>
-                                    {container.licencePlate}
-                                </td>
-                                <td>
-                                    {container.carrier}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
     )
 }
