880d72e15ed31854353953e38e00a8831d55454c
marcusdev99 <marcus.sea13@gmail.com>
2023-08-22 14:16:03 -0600

    feat : addition of Home component addition of component to home view -Navbar -ContainerInventory

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from './Navbar'
+import { ContainerInventory } from './ContainerInventory'
+
+export const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <ContainerInventory />
+        </>
+    )
+}
