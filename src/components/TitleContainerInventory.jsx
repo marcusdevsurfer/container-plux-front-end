5d234ef899e0bb6bf19d610b967d62227fb55658
marcusdev99 <marcus.sea13@gmail.com>
2023-08-22 14:13:58 -0600

    refactor : component unification creation of this component for divide inventory title in separated component

---
+++ src/components/TitleContainerInventory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TitleContainerInventory = () => {
+    return (
+        <div>
+            <h2 className='display-6 text-center'>Inventario</h2>
+        </div>
+    )
+}
