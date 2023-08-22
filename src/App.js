95701f3c5c8dc1a41600f535514fca2d7388c4ad
marcusdev99 <marcus.sea13@gmail.com>
2023-08-21 23:15:16 -0600

    feat : addition of react router dom dependency

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import { ReactDOM } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Navbar } from "./components/Navbar";
 import { ContainerInventory } from "./components/ContainerInventory";
 import { AddContainer } from "./components/AddContainer";
+import { Home } from "./components/Home";
 
 
 
 
 
 function App() {
-
-
   return (
-    <>
-    <Navbar/>
-    <ContainerInventory/>
-    <AddContainer/>
-    </>
-    );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home/>}/>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
