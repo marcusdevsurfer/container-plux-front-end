da7bf25c8e17e0f2be1de7f7ed39d54559c9a664
marcusdev99 <marcus.sea13@gmail.com>
2023-09-05 12:08:52 -0600

    refactor : addition of footer component

--- src/views/ContainersSummary.jsx
+++ src/views/ContainersSummary.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
 import {ContainerInventory} from '../components/ContainerInventory'
+import {Footer} from '../components/Footer'
 
 export const ContainersSummary = () => {
     return (
         <>
             <Navbar />
             <ContainerInventory/>
+            <Footer/>
         </>
     )
 }
