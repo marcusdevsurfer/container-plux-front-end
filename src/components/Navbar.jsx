d7f10b84f16ca0ed080064e6a56009702783799b
marcusdev99 <marcus.sea13@gmail.com>
2023-09-05 12:07:52 -0600

    refact : code changes Login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 
 export const Navbar = () => {
-  const navbarStyle = {
-    'height' : '7.5vh',
-    'margin' : '0',
-    'padding' : '0',
-    'background' : '#0F0E0E',
-    'color' : 'white'
 
-  }
   return (
-    <nav style={navbarStyle} className="navbar">
-        <div className="container-fluid">
-          <a href='/' className="navbar-brand text-white">
-            Container Plux
-          </a>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Container Plux</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="/containers-summary">Inventario</a>
+          </div>
         </div>
-      </nav>
+      </div>
+    </nav>
+
+
   )
 }
